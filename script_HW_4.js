35b244b942f213abb4506855e158f1e666ded01a
YorickMovr <yoricksamovar@gmail.com>
2019-08-10 08:36:29 +0300

    calculator

---
+++ script_HW_4.js
@@ -0,0 +1,27 @@
+alert('Hello! Here you can use our calculator');
+alert('Choose an operation');
+let operation = prompt('add for +, sub for -, mult for *, div for /');
+ switch(operation){
+     case 'add': addition();break;
+     case 'sub': subtraction
+ }
+
+ let operand1 = prompt('Ener first operand:');
+ let operand2 = prompt('Enter second operand:');
+
+ function addition(){
+     let sum = operand1 + operand2;
+     alert('Sum = ' + sum);
+ };
+
+ function subtraction(){
+
+ };
+
+ function multiplication(){
+
+ };
+
+ function division(){
+
+ };
