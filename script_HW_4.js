9256baf245f6a342d0181e57879357ab717ce305
YorickMovr <yoricksamovar@gmail.com>
2019-08-10 09:16:07 +0300

    ep

--- script_HW_4.js
+++ script_HW_4.js
@@ -1,13 +1,35 @@
-alert('Hello! Here you can use our calculator');
-alert('Choose an operation');
-let operation = prompt('add for +, sub for -, mult for *, div for /');
- switch(operation){
-     case 'add': addition();break;
-     case 'sub': subtraction
- }
+"use strict"
 
- let operand1 = prompt('Ener first operand:');
- let operand2 = prompt('Enter second operand:');
+alert('Hello! Here you can use our calculator');
+
+let operator;
+let operand1;
+let operand2;
+
+do {
+    operator = prompt('Choose operator', 'add');
+}while(
+        operator !='add' && 
+        operator !='sub' && 
+        operator !='mult' && 
+        operator !='div'
+        )
+
+do{
+     operand1 = +prompt('Ener first operand:');
+}while (isNaN(operand1));
+
+do{
+    operand2 = +prompt('Ener second operand:');
+}while (isNaN(operand2));
+
+
+switch(operator){
+    case 'add': addition();break;
+    case 'sub': subtraction(); break;
+    case 'mult': multiplication();break;
+    case 'div': division(); break;
+};
 
  function addition(){
      let sum = operand1 + operand2;
@@ -15,13 +37,16 @@ let operation = prompt('add for +, sub for -, mult for *, div for /');
  };
 
  function subtraction(){
-
+     let dif = operand1 - operand2;
+     alert('Difference = ' + dif);
  };
 
  function multiplication(){
-
+     let product = operand1 * operand2;
+     alert('Product = ' + product);
  };
 
  function division(){
-
+     let quotient = operand1 / operand2;
+     alert('Quotient = ' + quotient);
  };
