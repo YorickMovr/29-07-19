344dff771c703c8d7d57c0941eb89b6c7b62aa2d
YorickMovr <yoricksamovar@gmail.com>
2019-08-14 16:18:25 +0300

    l

--- Урок домашка 5/script_5.js
+++ Урок домашка 5/script_5.js
@@ -1,9 +1,9 @@
-function replace(string, char, replacer) {
+function replaceAll(string, char, replacer) {
     while(char.length > 1 || replacer.length > 1){
         alert ('Please check the values ​​of char and replacer, they should consist only one simbol each')
     };
     return string.split(char).join(replacer); 
   };
 
-  let str = replace('hello world!', 'll', 'z');
+  let str = replaceAll('hello world!', 'll', 'z');
   alert(str);
