da5af3575c0fd36f5ce0d4c49e345ae47c99f4e9
YorickMovr <yoricksamovar@gmail.com>
2019-08-15 17:10:23 +0300

    /

--- Занятие 5/script_5 new.js
+++ Занятие 5/script_5 new.js
@@ -1,1 +1,17 @@
-"use strict"
+// not this!!!
+
+// "use strict"
+// function replaceAll(str, findChar, replaceChar){
+//     if (findChar.length !==1 || replaceChar.length !==1){
+//         console.warn('wrong arguments');
+//         return str;
+//     }
+
+//     str = str.replace(findChar, replaceChar);
+
+//     return str.indexOf(findChar) >=0 ?
+//                replaceAll(str, findChar, replaceChar) : 
+//                str;
+// }
+
+// console.log(replaceAll('Hello world', 'l', 'z'));
