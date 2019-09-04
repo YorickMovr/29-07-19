c22842a33c6ece21a358c489cbe94c2d4410e92f
YorickMovr <yoricksamovar@gmail.com>
2019-09-05 01:44:42 +0300

    йцйцйцйцйц

--- Занятие 9/script.js
+++ Занятие 9/script.js
@@ -1,11 +1,39 @@
-document.getElementById('add').onclick = function(){
-    let d = document.getElementById('in').value;
+let list = document.querySelector('ul');
+const input = document.querySelector("input[type='text']");
 
-    let temp = [];
-    temp.toDo = d;
-    temp.check = false;
-    let i = toDoList.length
-    toDoList[i] = temp;
-    console.log(toDoList)
+list.addEventListener('click', function (ev) {
+    if(ev.target.tagName === "LI") {
+        ev.target.classList.toggle('checked');
+
+    } else if(ev.target.tagName === "SPAN") {
+       let div = ev.target.parentNode;
+       div.remove();
+    }
+}, false);
+
+
+input.addEventListener("keypress", (keyPressed) => {
+    const keyEnter = 13;
+    if (keyPressed.which == keyEnter){
+        newElement();
+    }
+})
+
+
+function newElement() {
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('toDoEl').value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue == "") {
+       alert("Введите ваше дело!");
+    } else {
+       document.getElementById('list').appendChild(li);
+    }
+    document.getElementById('toDoEl').value = "";
+    let span = document.createElement('SPAN');
+    let txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
 }
-
