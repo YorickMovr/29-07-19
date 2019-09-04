edbf4acc4a4dcb1a7661990205b6b48879ee34ea
YorickMovr <yoricksamovar@gmail.com>
2019-09-04 18:23:24 +0300

    new

--- Занятие 9/script.js
+++ Занятие 9/script.js
@@ -1,19 +1,11 @@
-const btm = document.getElementById('btn');
-const div = document.getElementById('div');
-const lis = document.querySelectorAll('#list > li');
+document.getElementById('add').onclick = function(){
+    let d = document.getElementById('in').value;
 
-for(let i=0; lis.length; i++){
-    lis[i].addEventListener('click', onClick);
+    let temp = [];
+    temp.toDo = d;
+    temp.check = false;
+    let i = toDoList.length
+    toDoList[i] = temp;
+    console.log(toDoList)
 }
 
-btn.addEventListener('click', onClick);
-div.addEventListener('click', onDivClick);
-
-function onClick(e){
-    console.log('clicked', e.target, this)
-    e.target.classList.add('clicked')
-};
-
-function onDivClick(e){
-    console.log('div', e.target, this);
-}
