4547c347d993ccf56aab881af776e8970a42fbf0
YorickMovr <yoricksamovar@gmail.com>
2019-09-12 20:54:18 +0300

    qs

--- Занятие 11/script.js
+++ Занятие 11/script.js
@@ -1,17 +1,34 @@
-function Animal(name){
+function Student(name, marks){
     this.name = name;
 
-         this.eat = function(){ console.log(`${this.name} is eaing`)
+    this.marks = marks;
+
+    this.eat = function(){ console.log( this.name + '  is eaing')}
+
+    this.averegeMark = function(){
+        console.log(this.marks)
+        
+    }
+        
 };
+
+const st1 =new Student('Student 1', [10,9,8,0,10]);
+
+const students = [ 
+    new Student('Student 1', [10,9,8,0,10]), // имя, оценки
+    new Student('Student 12', [10,0,8,0,3,4])
+   ];
+
+
+function avgMarks(name ,marks){
+    let total = 0;
+        for (let i =0; i< marks.length; i++){
+            total += marks[i];
+            };
+
+        let avg = total / marks.length;
+        console.log(name + '`s ' + '  avarage mark is ' + avg)
 };
 
-function Cat(name){
-    this.name = name;
-}
 
-Cat.prototype = new Animal;
 
-const dino = new Animal('dinozaur');
-const raptor = new Animal('Raptor');
-
-const kitty = new Cat('Kitty');
