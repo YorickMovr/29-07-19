8fe0d1b485c507d4b6b1547bfec432d2a805e0bf
YorickMovr <yoricksamovar@gmail.com>
2019-09-13 16:00:19 +0300

    sfd

--- Занятие 10/script.js
+++ Занятие 10/script.js
@@ -1,9 +1,21 @@
 function createCalculator(value = 0){
-    function add(a){
-        return value +=a;
-    };
-};
+    return {
+        add: (a)=> value +=a,
+        sub: (a)=> value -=a,
+        div: (a)=> value /=a,
+        mult: (a)=> value*=a,
+        reset: ()=> value =0,
+        set: (a)=> value = a,
+        value: ()=> value
+    }
+}
 
-const calculator = createCalculator(10);
 
-alert (add(5))
+
+const calculator = createCalculator(10); 
+calculator.add(45) // возвращает 55 
+calculator.sub(45) // возвращает 10 
+calculator.div(5) // возвращает 2 
+calculator.mult(5) // возвращает 10 
+calculator.set(100) // устанавливает базовое значение в 100 
+calculator.mult(5) // возвращает 500
