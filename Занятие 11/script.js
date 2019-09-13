2df944914d88b2ce627c8629acf70f6b0addf96d
YorickMovr <yoricksamovar@gmail.com>
2019-09-13 14:50:44 +0300

    в

--- Занятие 11/script.js
+++ Занятие 11/script.js
@@ -1,19 +1,9 @@
+
+
 function Student(name, marks){
 
     this.name = name;
     this.marks = marks;
-    
-
-    // this.averegeMarkForSt = function(){
-
-    // let total = 0;
-    //     for (let i =0; i< this.marks.length; i++){
-    //         total += this.marks[i];
-    //         };
-
-    //     let avg = total / this.marks.length;
-    //     console.log(`${name}` + '`s ' + 'avarage mark is ' + avg)
-    //     }
 };     
 
 Student.prototype.averegeMarkForSt = function(){
@@ -24,9 +14,30 @@ Student.prototype.averegeMarkForSt = function(){
 
 
 
+
+
 const st1 =new Student('Student 1', [10,9,8,0,10]);
 
-const students = [ 
+const studentsGroup = [ 
     new Student('Student 1', [10,9,8,0,10]), // имя, оценки
     new Student('Student 12', [10,0,8,0,3,4])
    ];
+
+// function averegeMarkForGroup (students){
+// let studentsMarks = []
+// for(let i=0; students.length < i; i++){
+//     studentsMarks.unshift(this.students.averegeMarkForSt())
+//     console.log(studentsMarks);
+
+// }
+//     //return studentsMarks;
+// }
+// function avg(students){
+// students.forEach(fuck);
+
+// }
+
+// function ck (students){
+// alert(students[0].averegeMarkForSt());
+// }
+
